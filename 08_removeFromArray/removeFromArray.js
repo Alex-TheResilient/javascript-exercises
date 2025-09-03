8bfcf27df28a0bcf8ec42e2dbf095af0d06b4b97
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-09-03 13:39:04 -0500

    refactor removeFromArray function and tests; enable all previously skipped tests

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+  const array = args[0];
+  const elementsToRemove = args.slice(1);
+  return array.filter((item) => !elementsToRemove.includes(item));
 };
 
 // Do not edit below this line
