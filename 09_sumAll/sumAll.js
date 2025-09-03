043820b757c4f2e32fc014730ff5724b2524ccfb
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-09-03 13:43:46 -0500

    refactor sumAll function and tests; enable all previously skipped tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR';
+  }
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return 'ERROR';
+  }
+  if (start > end) {
+    [start, end] = [end, start];
+  }
 
+  let sumAllNumbers = 0;
+  for (let i = start; i <= end; i++) {
+    sumAllNumbers += i;
+  }
+  return sumAllNumbers;
 };
 
 // Do not edit below this line
