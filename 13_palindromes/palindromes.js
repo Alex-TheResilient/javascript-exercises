631e3055037ff5167d0afef3fccf28e684dd7c76
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-09-03 13:56:13 -0500

    refactor palindromes function and tests; enable all previously skipped tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let cleaned = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  for (let i = 0; i < cleaned.length / 2; i++) {
+    if (cleaned[i] !== cleaned[cleaned.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
